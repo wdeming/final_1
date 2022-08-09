94a65314add63d19141e44fa175931040efa52d6
wdeming <57711720+wdeming@users.noreply.github.com>
2022-08-10 00:37:45 +0500

    programm

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿string[] array = new string[5];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine("Введите элементы массива: ");
+    array[i] = Console.ReadLine();
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + "\t");
+}
+Console.WriteLine();
+Console.WriteLine("Элементы из трёх символов: ");
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        Console.WriteLine(array[i] + "\t");
+    }
+}
+Console.WriteLine();
